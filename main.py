972f6d1c27b5e4532974bf44df8d5feef8839373
Oliver <kollo.oliver@gmail.com>
2022-04-26 17:27:33 +0100

    I added some documentation and condensed my code.

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ cap = cv2.VideoCapture(0)
 data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 def computer_choice():
+    '''
+    This function will generate a number between 1-3, which represents the computer choice in the game.
+    1 = rock, 2 = scissors, 3 = paper.
+    '''
     rng = random.randint(1,3)
     if rng == 1:
         X = 'Rock'
@@ -19,7 +23,16 @@ def computer_choice():
     print("The computer chose " + X + "!")
     return X
 
-def player_choice(prediction, accuracy = 0.5):
+def player_choice(accuracy = 0.5):
+    '''
+    This function will use the keras model and attempt to interpret your hand gesture into four outputs. 
+    These outputs are: Rock, Paper, Scissors and Nothing/Unknown in case it couldn't determine what hand gesture was made.
+    '''
+    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
+    image_np = np.array(resized_frame)
+    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 
+    data[0] = normalized_image
+    prediction = model.predict(data)
     if prediction[0][0] > accuracy:
         Y = "Rock"
     elif prediction[0][1] > accuracy:
@@ -27,11 +40,14 @@ def player_choice(prediction, accuracy = 0.5):
     elif prediction[0][2] > accuracy:
         Y = "Paper"
     else:
-        Y = "Unknown."
+        Y = "I did not catch that."
     print("You chose " + Y + "!")
     return Y
 
 def game_evaluation(X, Y):
+    '''
+    This function will evaluate based on the X and Y values from previous functions whether the computer or the player wins the round.
+    '''
     global player_win_count, computer_win_count
     if X == Y:
         print("Draw!")
@@ -44,17 +60,11 @@ def game_evaluation(X, Y):
     elif X == "Paper" and Y == "Scissor":
         print("You win!")
         player_win_count += 1
+    elif Y == 'Unknown':
+        print('I was not able to decipher your hand gesture!')
     else:
         print("Computer wins!")
         computer_win_count +=1
-
-def get_prediction(frame):
-    resized_frame = cv2.resize(frame, (224, 224), interpolation = cv2.INTER_AREA)
-    image_np = np.array(resized_frame)
-    normalized_image = (image_np.astype(np.float32) / 127.0) - 1 
-    data[0] = normalized_image
-    prediction = model.predict(data)
-    return prediction
 
 def countdown(t):
     while t:
@@ -67,7 +77,7 @@ def countdown(t):
     print('Rock Paper Scissor!!!')
 
 
-print("Welcome to rock, paper, scissors!")
+print("Welcome to my game of rock, paper, scissors!")
 answer = input("Do you want to play? Y/N ")
 if answer != "Y".lower():
     quit()
@@ -80,8 +90,7 @@ while True:
     current_time = time.time()
     ready = True
     if int(current_time - start_time)%8 == 0 and int(current_time - start_time) > 1 and ready:
-        prediction = get_prediction(frame)
-        game_evaluation(Y = player_choice(prediction), X = computer_choice())
+        game_evaluation(Y = player_choice(), X = computer_choice())
         print("Player score: ", player_win_count)
         print("Computer score: ", computer_win_count)
         ready = False
@@ -95,6 +104,5 @@ while True:
             break
     if cv2.waitKey(1) & 0xFF == ord('q'): 
         break
-
 cap.release()
 cv2.destroyAllWindows()
